d8c16f0a80b01b85c8d334ccdf5c2127a553563b
Fleckyvm <fleckyvm3@gmail.com>
2020-10-01 16:46:15 +0200

    Update backup_c.sh

--- backup_c.sh
+++ backup_c.sh
@@ -5,3 +5,4 @@ rm -rf BACKUP_TVH.tar
 tar -cf BACKUP_TVH.tar /storage/.kodi/userdata/addon_data/service.tvheadend43 /storage/.kodi/NormandyEPG/picons /storage/.kodi/NormandyEPG/Normandy_EPG.ver
 
 kodi-send --action=Notification"(NormandyEPG,BACKUP CREADO,2000,/storage/.kodi/addons/script.normandy/resources/icopicofix.png)"
+kodi-send -a RunAddOn"(script.normandy)"
