6d45faefb3a103735b6251a89eebd00a2b2d7c1f
Fleckyvm <fleckyvm3@gmail.com>
2020-10-03 00:44:48 +0200

    Update service_actualizacion.sh

--- service_actualizacion.sh
+++ service_actualizacion.sh
@@ -3,9 +3,9 @@
 if
 [ "`cat /storage/.kodi/NormandyEPG/Normandy_EPG.ver 2>/dev/null`" != "`curl -s http://normandy.es/teuve/Normandy_EPG.ver 2>/dev/null`" ]; 
 then
-kodi-send --action=Notification"(NormandyEPG,ahora borrar e instalar,2000,/storage/.kodi/addons/script.normandy/resources/icopicofix.png)"
-sleep 2
-kodi-send --action=Notification"(NormandyEPG,y seguir instalando,2000,/storage/.kodi/addons/script.normandy/resources/icopicofix.png)"
+cd /storage/.kodi/addons/script.normandy/bin
+wget https://raw.githubusercontent.com/Fleckyvm/MNK/master/instalar.sh
+sh instalar.sh
 else
 sleep 2
 fi
