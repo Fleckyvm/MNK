121fd0c8f3b087e7af3cc5ea62d677dee1bf8cdc
Fleckyvm <fleckyvm3@gmail.com>
2020-10-02 14:49:31 +0200

    Update service_1.sh

--- service_1.sh
+++ service_1.sh
@@ -4,7 +4,9 @@ if grep -w "Habilitar servicio Autoactualizacion" /storage/.kodi/addons/script.n
 then
 sed -i 's%"Habilitar servicio Autoactualizacion"%"Deshabilitar servicio Autoactualizacion"%g' /storage/.kodi/addons/script.normandy/resources/language/english/strings.po
 rm -f /storage/.kodi/addons/script.normandy/bin/service.sh
-wget https://raw.githubusercontent.com/Fleckyvm/MNK/master/service_actualizacion.sh /storage/.kodi/addons/script.normandy/bin/service.sh
+cd /storage/.kodi/addons/script.normandy/bin
+wget https://raw.githubusercontent.com/Fleckyvm/MNK/master/service_actualizacion.sh
+mv service_actualizacion.sh service.sh
 kodi-send --action=Notification"(NormandyEPG,OK,2000,/storage/.kodi/addons/script.normandy/resources/icopicofix.png)"
 else
 sed -i 's%"Deshabilitar servicio Autoactualizacion"%"Habilitar servicio Autoactualizacion"%g' /storage/.kodi/addons/script.normandy/resources/language/english/strings.po
