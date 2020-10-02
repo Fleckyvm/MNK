0d80b8df3d831c23791ca99518f6a0b83e740a90
Fleckyvm <fleckyvm3@gmail.com>
2020-10-02 14:17:30 +0200

    Add files via upload

---
+++ service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if
+[ "`cat /storage/.kodi/NormandyEPG/Normandy_EPG.ver 2>/dev/null`" != "`curl -s http://normandy.es/teuve/Normandy_EPG.ver 2>/dev/null`" ]; 
+then
+kodi-send --action=Notification"(NormandyEPG,DISPONIBLE ACTUALIZACION EPG,2000,/storage/.kodi/addons/script.normandy/resources/icopicofix.png)"
+sleep 2
+kodi-send --action=Notification"(NormandyEPG,ACTUALICE DESDE EL ADDON,2000,/storage/.kodi/addons/script.normandy/resources/icopicofix.png)"
+else
+sleep 2
+fi
