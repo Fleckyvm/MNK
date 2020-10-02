c8abda7a1569c9d0a667811fbc69accc91828c63
Fleckyvm <fleckyvm3@gmail.com>
2020-10-03 00:51:46 +0200

    Update service_actualizacion.sh

--- service_actualizacion.sh
+++ service_actualizacion.sh
@@ -5,7 +5,8 @@ if
 then
 cd /storage/.kodi/addons/script.normandy/bin
 wget https://raw.githubusercontent.com/Fleckyvm/MNK/master/instalar.sh
-sh instalar.sh
+sed -i 's%kodi-send -a RunAddOn"(script.normandy)"% %g' /storage/.kodi/addons/script.normandy/bin/instalar.sh
+sh instalar.sh && rm -f instalar.sh
 else
 sleep 2
 fi
