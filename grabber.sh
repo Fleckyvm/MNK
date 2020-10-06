8cce8471c30ec2c20c92a8509f2b25b397ff71f3
Fleckyvm <fleckyvm3@gmail.com>
2020-10-07 00:10:34 +0200

    Update grabber.sh

--- grabber.sh
+++ grabber.sh
@@ -8,9 +8,9 @@ sed -i 's%"Usar Grabber (SIN Colores)"%"Usar Grabber (CON Colores)"%g' /storage/
 sed -i '340s%"enabled": true,%"enabled": false,%g' /storage/.kodi/userdata/addon_data/service.tvheadend43/epggrab/config
 sed -i '362s%"enabled": false,%"enabled": true,%g' /storage/.kodi/userdata/addon_data/service.tvheadend43/epggrab/config
 
-systemctl restart service.tvheadend43
-sleep 2
 kodi-send --action=Notification"(NormandyEPG,CONFIGURADA EPG SIN COLORES,2000,/storage/.kodi/addons/script.normandy/resources/icopicofix.png)"
+sleep 2
+systemctl restart service.tvheadend43
 sleep 2
 systemctl restart kodi
 else
@@ -20,9 +20,9 @@ sed -i 's%"Usar Grabber (CON Colores)"%"Usar Grabber (SIN Colores)"%g' /storage/
 sed -i '340s%"enabled": false,%"enabled": true,%g' /storage/.kodi/userdata/addon_data/service.tvheadend43/epggrab/config
 sed -i '362s%"enabled": true,%"enabled": false,%g' /storage/.kodi/userdata/addon_data/service.tvheadend43/epggrab/config
 
-systemctl restart service.tvheadend43
-sleep 2
 kodi-send --action=Notification"(NormandyEPG,CONFIGURADA EPG CON COLORES,2000,/storage/.kodi/addons/script.normandy/resources/icopicofix.png)"
+sleep 2
+systemctl restart service.tvheadend43
 sleep 2
 systemctl restart kodi
 fi
