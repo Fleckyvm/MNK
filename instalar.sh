257491c9d65c11e9646ceaa368d654bba9263fd5
Fleckyvm <fleckyvm3@gmail.com>
2020-10-07 00:52:55 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -58,6 +58,5 @@ sleep 3
 sed -i 's%"/usr/bin/tv_grab_file": {%"/storage/.kodi/addons/service.tvheadend43/bin/tv_grab_file": {%g' /storage/.kodi/userdata/addon_data/service.tvheadend43/epggrab/config
 sed -i 's%"/usr/bin/tv_grab_NormandyEPGnm": {%"/storage/.kodi/addons/service.tvheadend43/bin/tv_grab_NormandyEPGnm": {%g' /storage/.kodi/userdata/addon_data/service.tvheadend43/epggrab/config
 sed -i 's%"/usr/bin/tv_grab_NormandyEPGmv": {%"/storage/.kodi/addons/service.tvheadend43/bin/tv_grab_NormandyEPGmv": {%g' /storage/.kodi/userdata/addon_data/service.tvheadend43/epggrab/config
-sh /storage/.kodi/addons/service.tvheadend43/bin/tv_grab_NormandyEPGnm
 systemctl restart service.tvheadend43
 kodi-send -a RunAddOn"(script.normandy)"
