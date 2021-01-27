313ca232bdd2a3b2b49795f407b3220c19610b60
Fleckyvm <fleckyvm3@gmail.com>
2021-01-27 14:31:18 +0100

    Update vincular.sh

--- vincular.sh
+++ vincular.sh
@@ -19,15 +19,15 @@ cp -r /storage/.kodi/addons/script.normandy/bin/oscam.server /storage/.kodi/user
 cp -r /storage/.kodi/addons/script.normandy/bin/oscam.user /storage/.kodi/userdata/addon_data/service.softcam.oscam/config/oscam.user
 kodi-send --action=Notification"(NormandyEPG,2CONEXION ESTABLECIDA TVH-OSCAM,2000,/storage/.kodi/addons/script.normandy/resources/icopicofix.png)"
 sleep 3
+systemctl restart service.softcam.oscam
+sleep 1
+systemctl restart service.tvheadend43
+fi
+sleep 3
 wget https://github.com/NormandyEPG/Picons-NormandyEPG/archive/master.zip
 mkdir -p /storage/.kodi/NormandyEPG /storage/.kodi/NormandyEPG/tmp-tpl
 unzip /storage/master.zip -d /storage/.kodi/NormandyEPG/tmp-tpl
 cp -r /storage/.kodi/NormandyEPG/tmp-tpl/Picons-NormandyEPG-master/tpl /storage/.kodi/NormandyEPG
 rm -rf /storage/.kodi/NormandyEPG/tmp-tpl
 rm -rf /storage/master.zip
-sleep 3
-systemctl restart service.softcam.oscam
-sleep 1
-systemctl restart service.tvheadend43
-fi
 kodi-send -a RunAddOn"(script.normandy)"
